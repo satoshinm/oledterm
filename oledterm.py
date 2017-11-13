aba7682c8278ddb2c11cc94c44aaaacad8f4173a
satoshinm <snmatsutake@yahoo.co.jp>
2017-11-13 02:17:21 +0000

    Always use tiny font

--- oledterm.py
+++ oledterm.py
@@ -50,59 +50,60 @@ def make_font(name, size):
 
 def main():
     while True:
-        #for fontname, size in [(None, None), ("tiny.ttf", 6), ("ProggyTiny.ttf", 16), ("creep.bdf", 16), ("miscfs_.ttf", 12), ("FreePixel.ttf", 12)]:
-        for fontname, size in [("tiny.ttf", 6), ("ProggyTiny.ttf", 16), ("creep.bdf", 16), ("miscfs_.ttf", 12), ("FreePixel.ttf", 12)]:
-            font = make_font(fontname, size) if fontname else None
-            term = terminal(device, font)
+        fontname = "tiny.ttf"
+        size = 6
 
-            term.println("Terminal mode demo")
-            term.println("------------------")
-            term.println("Uses any font to output text using a number of different print methods.")
-            term.println()
-            time.sleep(2)
-            term.println("The '{0}' font supports a terminal size of {1}x{2} characters.".format(fontname, term.width, term.height))
-            term.println()
-            time.sleep(2)
-            term.println("An animation effect is defaulted to give the appearance of spooling to a teletype device.")
-            term.println()
-            time.sleep(2)
+        font = make_font(fontname, size) if fontname else None
+        term = terminal(device, font)
 
-            term.println("".join(chr(i) for i in range(32, 127)))
-            time.sleep(2)
+        term.println("Terminal mode demo")
+        term.println("------------------")
+        term.println("Uses any font to output text using a number of different print methods.")
+        term.println()
+        time.sleep(2)
+        term.println("The '{0}' font supports a terminal size of {1}x{2} characters.".format(fontname, term.width, term.height))
+        term.println()
+        time.sleep(2)
+        term.println("An animation effect is defaulted to give the appearance of spooling to a teletype device.")
+        term.println()
+        time.sleep(2)
 
-            term.clear()
-            for i in range(30):
-                term.println("Line {0:03d}".format(i))
+        term.println("".join(chr(i) for i in range(32, 127)))
+        time.sleep(2)
 
-            term.animate = False
-            time.sleep(2)
-            term.clear()
+        term.clear()
+        for i in range(30):
+            term.println("Line {0:03d}".format(i))
 
-            term.println("Progress bar")
-            term.println("------------")
-            for mill in range(0, 10001, 25):
-                term.puts("\rPercent: {0:0.1f} %".format(mill / 100.0))
-                term.flush()
+        term.animate = False
+        time.sleep(2)
+        term.clear()
 
-            time.sleep(2)
-            term.clear()
-            term.puts("Backspace test.")
+        term.println("Progress bar")
+        term.println("------------")
+        for mill in range(0, 10001, 25):
+            term.puts("\rPercent: {0:0.1f} %".format(mill / 100.0))
             term.flush()
-            time.sleep(2)
-            for _ in range(17):
-                term.backspace()
-                time.sleep(0.2)
 
-            time.sleep(2)
-            term.clear()
-            term.animate = True
-            term.println("Tabs test")
-            term.println("|...|...|...|...|...|")
-            term.println("1\t2\t4\t11")
-            term.println("992\t43\t9\t12")
-            term.println("\t3\t99\t1")
-            term.flush()
-            time.sleep(2)
+        time.sleep(2)
+        term.clear()
+        term.puts("Backspace test.")
+        term.flush()
+        time.sleep(2)
+        for _ in range(17):
+            term.backspace()
+            time.sleep(0.2)
+
+        time.sleep(2)
+        term.clear()
+        term.animate = True
+        term.println("Tabs test")
+        term.println("|...|...|...|...|...|")
+        term.println("1\t2\t4\t11")
+        term.println("992\t43\t9\t12")
+        term.println("\t3\t99\t1")
+        term.flush()
+        time.sleep(2)
 
 
 if __name__ == "__main__":
