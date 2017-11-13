3e65f62bae8292084f22887ba287c43ee03a7311
satoshinm <snmatsutake@yahoo.co.jp>
2017-11-13 03:00:38 +0000

    Read from Linux virtual console

--- oledterm.py
+++ oledterm.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# based on:
 # Copyright (c) 2014-17 Richard Hull and contributors
 # See LICENSE.rst for details.
 # PYTHON_ARGCOMPLETE_OK
-
-"""
-Simple println capabilities.
-"""
 
 import os
 import time
@@ -14,6 +11,10 @@ import sys
 from luma.core import cmdline
 from luma.core.virtual import terminal
 from PIL import ImageFont
+
+VIRTUAL_TERMINAL_DEVICE = "/dev/vcsa"
+ROWS = 9
+COLS = 28
 
 # based on demo_opts.py
 from luma.core import cmdline, error
@@ -49,60 +50,23 @@ def make_font(name, size):
 
 
 def main():
+    if not os.access(VIRTUAL_TERMINAL_DEVICE, os.R_OK):
+       print "Unable to access %s, try running as root?" % (VIRTUAL_TERMINAL_DEVICE,)
+       raise SystemExit
+
+    fontname = "tiny.ttf"
+    size = 6
+
+    font = make_font(fontname, size) if fontname else None
+    term = terminal(device, font, animate=False)
+
+    term.println("oledterm starting...")
+    time.sleep(1)
+
     while True:
-        fontname = "tiny.ttf"
-        size = 6
-
-        font = make_font(fontname, size) if fontname else None
-        term = terminal(device, font, animate=False)
-
-        term.println("oledterm starting...")
-
-        term.println("------------------")
-        term.println("Uses any font to output text using a number of different print methods.")
-        term.println()
-        time.sleep(2)
-        term.println("The '{0}' font supports a terminal size of {1}x{2} characters.".format(fontname, term.width, term.height))
-        term.println()
-        time.sleep(2)
-        term.println("An animation effect is defaulted to give the appearance of spooling to a teletype device.")
-        term.println()
-        time.sleep(2)
-
-        term.println("".join(chr(i) for i in range(32, 127)))
-        time.sleep(2)
-
+        data = file(VIRTUAL_TERMINAL_DEVICE).read()
         term.clear()
-        for i in range(30):
-            term.println("Line {0:03d}".format(i))
-
-        term.animate = False
-        time.sleep(2)
-        term.clear()
-
-        term.println("Progress bar")
-        term.println("------------")
-        for mill in range(0, 10001, 25):
-            term.puts("\rPercent: {0:0.1f} %".format(mill / 100.0))
-            term.flush()
-
-        time.sleep(2)
-        term.clear()
-        term.puts("Backspace test.")
-        term.flush()
-        time.sleep(2)
-        for _ in range(17):
-            term.backspace()
-            time.sleep(0.2)
-
-        time.sleep(2)
-        term.clear()
-        term.animate = True
-        term.println("Tabs test")
-        term.println("|...|...|...|...|...|")
-        term.println("1\t2\t4\t11")
-        term.println("992\t43\t9\t12")
-        term.println("\t3\t99\t1")
+        term.println(data)
         term.flush()
         time.sleep(2)
 
