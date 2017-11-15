3232d3a6f2384b3416a73f4ed358a45684b5d46a
satoshinm <snmatsutake@yahoo.co.jp>
2017-11-15 06:38:55 +0000

    Set cols/rows with stty on virtual console

--- oledterm.py
+++ oledterm.py
@@ -72,6 +72,8 @@ def main():
 
 
 if __name__ == "__main__":
+    os.system("stty rows %d" % (ROWS,))
+    os.system("stty cols %d" % (COLS,)) 
     try:
         device = get_device()
         main()
